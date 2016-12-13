421fbf5973fb9972454f85a448733ec96aaaa8d7
mohamed786 <mubashirarshad786@gmail.com>
2016-12-13 23:35:59 +0530

    aospa nougat

---
+++ BoardConfig.mk
@@ -0,0 +1,99 @@
+
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# inherit from common msm8930
+-include device/samsung/msm8930-common/BoardConfigCommon.mk
+
+TARGET_SPECIFIC_HEADER_PATH := device/samsung/loganreltexx/include
+
+# Kernel
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=22 zcache msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1
+BOARD_KERNEL_BASE := 0x80200000
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
+BOARD_KERNEL_PAGESIZE := 2048
+TARGET_KERNEL_SOURCE := kernel/samsung/msm8930-common
+TARGET_KERNEL_CONFIG := cyanogenmod_loganre_defconfig
+TARGET_KERNEL_VARIANT_CONFIG := msm8930_loganre_eur_lte_defconfig
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := MSM8960
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := loganreltexx,loganrelte,GT-S7275R,GT-S7275B,GT-S7275T
+
+# Recovery
+TARGET_RECOVERY_FSTAB := device/samsung/loganreltexx/rootdir/fstab.qcom
+
+# Filesystem
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 10485760
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 10485760
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1698693120
+BOARD_SYSTEMIMAGE_JOURNAL_SIZE := 0
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 5693733888
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_CACHEIMAGE_PARTITION_SIZE := 209715200
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/loganreltexx/bluetooth
+
+# NFC
+#BOARD_HAVE_NFC := true
+#BOARD_NFC_CHIPSET := pn547
+#BOARD_NFC_LPM_LOSES_CONFIG := true
+
+# Charger
+BOARD_NO_CHARGER_LED := true
+
+# Audio
+USE_CUSTOM_AUDIO_POLICY := 1
+
+# Needed for LPA
+BOARD_GLOBAL_CFLAGS += -DLPA_DEFAULT_BUFFER_SIZE=32
+
+# FM
+AUDIO_FEATURE_ENABLED_FM_POWER_OPT := true
+TARGET_FM_LEGACY_PATCHLOADER := true
+TARGET_QCOM_NO_FM_FIRMWARE := false
+
+# Allow suspend in charge mode
+BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_CHARGER_SHOW_PERCENTAGE := true
+
+# Camera
+TARGET_NEED_DISABLE_AUTOFOCUS := true
+TARGET_NEED_DISABLE_FACE_DETECTION := true
+TARGET_NEED_DISABLE_FACE_DETECTION_BOTH_CAMERAS := true
+
+# RIL
+TARGET_RIL_VARIANT := caf
+BOARD_GLOBAL_CFLAGS += -DDISABLE_ASHMEM_TRACKING
+BOARD_RIL_CLASS := ../../../device/samsung/loganreltexx/ril/
+
+# Time service
+BOARD_USES_QC_TIME_SERVICES := true
+
+# CMHW
+BOARD_HARDWARE_CLASS += device/samsung/loganreltexx/cmhw
---
+++ system_prop.mk
@@ -0,0 +1,89 @@
+# Set composition for USB
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp
+
+# RIL
+PRODUCT_PROPERTY_OVERRIDES += \
+    rild.libpath=/system/lib/libsec-ril.so \
+    rild.libargs=-d/dev/smd0 \
+    telephony.lteOnGsmDevice=1 \
+    ro.telephony.default_network=3 \
+    ro.telephony.ril_class=LoganreRIL \
+    ro.ril.telephony.mqanelements=6 \
+    persist.radio.add_power_save=1
+
+# GPS
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.gps.qmienabled=true \
+    persist.gps.qc_nlp_in_use=1 \
+    ro.qc.sdk.izat.premium_enabled=0 \
+    ro.qc.sdk.izat.service_mask=0x0 \
+    ro.gps.agps_provider=1
+
+# Media
+PRODUCT_PROPERTY_OVERRIDES += \
+    audio.offload.disable=1 \
+    mm.enable.smoothstreaming=true \
+    use.dedicated.device.for.voip=true \
+    use.voice.path.for.pcm.voip=true \
+    media.aac_51_output_enabled=true \
+    media.stagefright.legacyencoder=true \
+    media.stagefright.less-secure=true
+
+# Graphics
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.composition.type=c2d \
+    persist.debug.wfd.enable=1 \
+    persist.sys.wfd.virtual=0 \
+    ro.sf.lcd_density=240 \
+    ro.opengles.version=196608 \
+    ro.qualcomm.cabl=0
+
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+    camera2.portability.force_api=1
+
+# Misc
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.chipname=MSM8930 \
+    ro.product_ship=true \
+    ro.warmboot.capability=1 \
+    persist.timed.enable=true \
+    wifi.interface=wlan0 \
+    ro.qualcomm.bt.hci_transport=smd \
+    qcom.bluetooth.soc=smd \
+    ro.sys.sdcardfs=true \
+    camera.disable_zsl_mode=1
+    
+# QC Perf
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.extension_library=libqti-perfd-client.so
+
+# Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dex2oat-swap=true \
+    ro.am.reschedule_service=true \
+    ro.sys.fw.dex2oat_thread_count=2
+
+# Our low-ram optimizations below
+
+# Art
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dex2oat-flags=--no-watch-dog \
+    dalvik.vm.dex2oat-Xms=64m \
+    dalvik.vm.dex2oat-Xmx=384m \
+    dalvik.vm.dex2oat-threads=2
+
+# Low-RAM optimizations
+ADDITIONAL_BUILD_PROPERTIES += \
+    config.disable_atlas=true
+
+# Intel's VM tunable recommendations
+ADDITIONAL_BUILD_PROPERTIES += \
+    dalvik.vm.heapgrowthlimit=64m \
+    dalvik.vm.heapsize=174m \
+    dalvik.vm.heapmaxfree=2m
+     
+# OpenGLRenderer Optimizations
+ADDITIONAL_BUILD_PROPERTIES += \
+    ro.hwui.text_large_cache_height=1024
